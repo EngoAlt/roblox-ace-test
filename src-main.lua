29e255a5411d99eeeb5f6367dd9648040e1e946b
EngoAlt <100957468+EngoAlt@users.noreply.github.com>
2022-06-13 18:20:22 +0100

    Update src-main.lua

--- src-main.lua
+++ src-main.lua
@@ -1,8 +1,10 @@
-local renv = getrenv
-local genv = getgenv
+if game.PlaceId == 9620604224 then 
+    local renv = getrenv
+    local genv = getgenv
 
-if not renv then game.Players.LocalPlayer:Kick("\nNo renv, ew!") return end
-if not genv then game.Players.LocalPlayer:Kick("\nNo genv, ew!") return end
+    if not renv then game.Players.LocalPlayer:Kick("\nNo renv, ew!") return end
+    if not genv then game.Players.LocalPlayer:Kick("\nNo genv, ew!") return end
 
-renv()["_G"].getgenv = genv
-print("Succesfully allowed stupidity!")
+    renv()["_G"].getgenv = genv
+    print("Succesfully allowed stupidity!")
+end
