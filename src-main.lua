e4ae40676db413aa521583602e8a45de8387b6cb
EngoAlt <100957468+EngoAlt@users.noreply.github.com>
2022-06-13 18:15:16 +0100

    Create src-main.lua

---
+++ src-main.lua
@@ -0,0 +1,8 @@
+local renv = getrenv
+local genv = getgenv
+
+if not renv then game.Players.LocalPlayer:Kick("\nNo renv, ew!") return end
+if not genv then game.Players.LocalPlayer:Kick("\nNo genv, ew!") return end
+
+renv()["_G"].getgenv = genv
+print("Succesfully allowed stupidity!")
